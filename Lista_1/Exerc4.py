03c9de3d2968f4a9613e418987bf643cb44b71e2
FKettl <fekettl@gmail.com>
2021-11-14 21:36:53 -0300

    Algumas melhorias

--- Lista_1/Exerc4.py
+++ Lista_1/Exerc4.py
@@ -18,21 +18,26 @@ class Usuario:
         self.__senha = senha
         Usuario.lista_de_usuarios.append(self)
     
+
     @property
     def nome(self):
         return self.__nome
     
+
     @nome.setter
     def nome(self, novonome):
         self.__nome = novonome
+
 
     @property
     def senha(self):
         return self.__senha
 
+
     @senha.setter
     def senha(self, novasenha):
         self.__senha = novasenha
+
 
     def deletar_usuario(self):
         index = Usuario.lista_de_usuarios.index(self)
@@ -47,8 +52,9 @@ class Sistema:
         self.login = False
         self.livroaberto = False
 
+
     def logar(self, usuario, senha):
-        #Sistema.atualizar_Tela()
+        Sistema.atualizar_Tela()
         if self.login == True:
             print('Um usuário ja está logado')
         else:
@@ -58,12 +64,15 @@ class Sistema:
                     print('login com sucesso')
                     break
             if self.login == False:
-                print('Usuário não encontrado')
+                print('Usuário e/ou senha estão errados')
 
 
     def deslogar(self):
-        self.login = False
-    
+        if self.login == True:
+            self.login = False
+        else:
+            print('Não há um usuário logado')    
+
     @staticmethod
     def listar_livros():
         Livro.exibir_storage()
@@ -75,11 +84,13 @@ class Sistema:
 
         else:
             Sistema.atualizar_Tela()
-            livro = int(input('Escolha um livro(index da lista ou nome): '))
+            livro = input('Escolha um livro(index da lista ou nome): ')
             if livro.isnumeric():
+                livro = int(livro)
                 '''Mostra o livro'''
                 Sistema.atualizar_Tela()
                 self.livroaberto = True
+                #aqui vai retorna só o objeto, no sistema verdadeiro ia dar pra acessar os atributos, incluindo conteudo 
                 return Livro.storage[livro]
             else:
                 for x in Livro.storage:
@@ -87,23 +98,37 @@ class Sistema:
                         Sistema.atualizar_Tela()
                         self.livroaberto = True
                         '''Mostra o livro'''
-                        return livro
+                        #aqui vai retorna só o objeto, no sistema verdadeiro ia dar pra acessar os atributos, incluindo conteudo²
+                        return x
                 print('Livro não encontrado, tente novamente')
 
-    def fechar_livro(self):
-        self.livroaberto = False
-        Sistema.atualizar_Tela()
-        '''Fecha o livro'''
 
+    def fechar_livro(self):
+        if self.livroaberto == True:
+            self.livroaberto = False
+            Sistema.atualizar_Tela()
+            '''Fecha o livro'''
+        else:
+            print('Não há um livro aberto')
+    
 
     def proxima_pagina():
+        #não coloquei condição de haver um livro aberto pois teoricamente só tem como acessar essa função na tela do livro ja
         Sistema.atualizar_Tela()
         '''Avança a pagina do livro'''
 
+
     def voltar_pagina():
+        #não coloquei condição de haver um livro aberto pois teoricamente só tem como acessar essa função na tela do livro ja²
         Sistema.atualizar_Tela()
         '''Volta a pagina do livro'''
+
 
     def atualizar_Tela():
         '''Pega a ação e atualiza a tela com base nela'''
         print('Tela mudou')
+
+
+os1 = Sistema('Meu livro')
+os1.listar_livros()
+print(os1.escolher_livro())
