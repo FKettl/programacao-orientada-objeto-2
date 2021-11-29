ee042cae0443469863f04b2919670c035ea47a73
FKettl <fekettl@gmail.com>
2021-11-29 20:44:49 -0300

    Exerc 1 e 2 da lista 3 e dois txt necessários pros exerc

---
+++ Lista_3/Exerc1.py
@@ -0,0 +1,21 @@
+#Eu ia utilizar regex, porém troquei regex por lógica básica pra fazer a função certinho
+
+def contar_ocorrencia(lista):
+    dicionario = {}
+    for x in lista:
+        if x in dicionario:
+            dicionario[x] += 1
+        else:
+            dicionario[x] = 1
+    return dicionario
+
+f = open('C:/1Principal/Projects/Python/POO2/Lista_3/texto.txt', 'r',encoding='utf-8')
+conteudo = f.read().lower()
+conteudo = conteudo.replace(',', '')
+conteudo = conteudo.replace('.', '')
+lista = conteudo.split()
+dicionario = contar_ocorrencia(lista)
+ordenado = sorted(dicionario.items(), key=lambda x: x[1], reverse=True)
+print(dicionario)
+#Pra ver uma lista de tupla com (palavra, frequencia dela) em ordem decrescente
+#print(ordenado)
---
+++ Lista_3/Exerc2.py
@@ -0,0 +1,9 @@
+from Exerc1 import dicionario
+
+print(len(dicionario))   
+e = open('C:/1Principal/Projects/Python/POO2/Lista_3/stopwords.txt', 'r', encoding='utf-8')
+stopwords = e.read().lower().split()
+for x in stopwords:
+    if x in dicionario:
+        del dicionario[x]
+print(dicionario)
